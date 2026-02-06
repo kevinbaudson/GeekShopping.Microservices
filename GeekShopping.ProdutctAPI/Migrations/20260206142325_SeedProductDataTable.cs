dee8b0c6884a18a3a5f7d3339ffd06e817aa4d9b
KevinBaudson <kevinbaudson@hotmail.com>
2026-02-06 11:40:47 -0300

    feat(database): adiciona seed inicial de produtos via EF Core

---
+++ GeekShopping.ProdutctAPI/Migrations/20260206142325_SeedProductDataTable.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GeekShopping.ProdutctAPI.Migrations
+{
+    public partial class SeedProductDataTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "product",
+                columns: new[] { "id", "category_name", "description", "image_url", "name", "price" },
+                values: new object[] { 5L, "Material Escolar", "Caderno de 100 folhas", "https://m.media-amazon.com/images/I/61n9s+qj8eL._AC_SX679_.jpg", "Caderno", 19.9m });
+
+            migrationBuilder.InsertData(
+                table: "product",
+                columns: new[] { "id", "category_name", "description", "image_url", "name", "price" },
+                values: new object[] { 6L, "Material Escolar", "Caneta esferográfica azul", "https://m.media-amazon.com/images/I/61n9s+qj8eL._AC_SX679_.jpg", "Caneta", 5.9m });
+
+            migrationBuilder.InsertData(
+                table: "product",
+                columns: new[] { "id", "category_name", "description", "image_url", "name", "price" },
+                values: new object[] { 7L, "Acessórios", "Mochila resistente para o dia a dia", "https://m.media-amazon.com/images/I/61n9s+qj8eL._AC_SX679_.jpg", "Mochila", 99.9m });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "id",
+                keyValue: 5L);
+
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "id",
+                keyValue: 6L);
+
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "id",
+                keyValue: 7L);
+        }
+    }
+}
--- GeekShopping.ProdutctAPI/Model/Context/MySQLContext.cs
+++ GeekShopping.ProdutctAPI/Model/Context/MySQLContext.cs
@@ -7,5 +7,38 @@ namespace GeekShopping.ProdutctAPI.Model.Context
         public MySQLContext(){}
         public MySQLContext(DbContextOptions<MySQLContext> options) : base(options){}
         public DbSet<Product> Products { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasData(
+                new Product
+                {
+                    Id = 5,
+                    Name = "Caderno",
+                    Price = new decimal(19.90),
+                    Description = "Caderno de 100 folhas",
+                    CategoryName = "Material Escolar",
+                    ImageUrl = "https://m.media-amazon.com/images/I/61n9s+qj8eL._AC_SX679_.jpg"
+                },
+                new Product
+                {
+                    Id = 6,
+                    Name = "Caneta",
+                    Price = new decimal(5.90),
+                    Description = "Caneta esferográfica azul",
+                    CategoryName = "Material Escolar",
+                    ImageUrl = "https://m.media-amazon.com/images/I/61n9s+qj8eL._AC_SX679_.jpg"
+                },
+                new Product
+                {
+                    Id = 7,
+                    Name = "Mochila",
+                    Price = new decimal(99.90),
+                    Description = "Mochila resistente para o dia a dia",
+                    CategoryName = "Acessórios",
+                    ImageUrl = "https://m.media-amazon.com/images/I/61n9s+qj8eL._AC_SX679_.jpg"
+                }
+            );
+        }
     }
 }
